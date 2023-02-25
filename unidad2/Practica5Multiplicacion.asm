c8acf32a5f73e22bd1647e7fbbbd02d35cf3b286
Manuel Bautista <garcia.manuel.b7@gmail.com>
2023-02-24 21:51:36 -0600

    Agregar practica 5 de la unidad 2

--- unidad2/Practica5Multiplicacion.asm
+++ unidad2/Practica5Multiplicacion.asm
@@ -2,23 +2,22 @@ SECTION .bss
     RESULTADO resb 1
 
 SECTION .text
-    global _start          
+    GLOBAL _start
 
 _start:
-    MOV BX, 2
-    MOV DX, 0
-    MOV AX, 8
 
-    DIV BX 
+    MOV AX, 3, 
+    MOV CX, 2
+
+    MUL CX 
 
     ADD AX, 48
     MOV [RESULTADO], AX
-
-
+    
     MOV EAX, 4
     MOV EBX, 1
-    MOV ECX, RESULTADO 
-    MOV EDX, 1
+    MOV ECX, RESULTADO
+    MOV EDX, 2
     INT 0x80
 
     MOV EAX, 1
